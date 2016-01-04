ab91acdffa455fc53b8baded6c06ca4fa9088789
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2016-01-04 21:30:47 +0100

    menu e primi contenuti

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,18 @@
 //= require jquery
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', function(){
+var elements = document.querySelectorAll('[data-class-toggle]');
+
+  Array.prototype.forEach.call(elements, function(el, i) {
+    var classToToggle = el.getAttribute('data-class-toggle');
+    var elToToggle = document.querySelector(el.getAttribute('data-element'));
+
+    el.addEventListener('click', function(e) {
+      var method = elToToggle.classList.contains(classToToggle) ? 'remove' : 'add';
+      elToToggle.classList[method](classToToggle);
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
+});
