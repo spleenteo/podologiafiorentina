ab4b89c8001609664911cc9d11acabab568354cf
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2016-02-21 16:59:37 +0100

    strafuck

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,17 +1,5 @@
 //= require jquery
-//= require jquery.isotope
-//= require_tree .
+//= require isotope.pkgd.min
+//= require isotope_layout
 
-function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam){
-            return sParameterName[1];
-        }else{
-          return false;
-        }
-    }
-}
+
---
+++ source/javascripts/isotope_layout.js
@@ -0,0 +1,78 @@
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam){
+            return sParameterName[1];
+        }else{
+          return false;
+        }
+    }
+}
+
+
+
+$(document).ready(function(){
+
+  var elements = document.querySelectorAll('[data-class-toggle]');
+
+  Array.prototype.forEach.call(elements, function(el, i) {
+    var classToToggle = el.getAttribute('data-class-toggle');
+    var elToToggle = document.querySelector(el.getAttribute('data-element'));
+
+    el.addEventListener('click', function(e) {
+      var method = elToToggle.classList.contains(classToToggle) ? 'remove' : 'add';
+      elToToggle.classList[method](classToToggle);
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
+
+
+  var $container = $('.js-isotope')
+
+  $container.isotope({
+    // main isotope options
+    itemSelector: '.js-isotope__item',
+    layoutMode: 'fitRows',
+    masonry: {
+      columnWidth: ".js-isotope__item"
+    }
+  })
+
+  filter = getUrlParameter("filter");
+  if(filter != false){
+    $(".js-filter-title").show();
+    $(".js-filter-name").html(filter.replace("-"," "));
+
+    $container.isotope({ filter: '.'+filter });
+  }else{
+    $container.isotope({ filter: '*' });
+  }
+});
+
+
+
+// var $container = document.querySelector('.js-isotope');
+// if ($container) {
+//   $i = new Isotope( $container, {
+//     itemSelector: '.js-isotope__item',
+//     transitionDuration: '0.2s',
+//     layoutMode: 'fitRows',
+//     masonry: {
+//       columnWidth: '.js-isotope__item'
+//     }
+//   });
+// }
+
+// filter = getUrlParameter("filter");
+// if(filter != false){
+//   $(".js-filter-title").show();
+//   $(".js-filter-name").html(filter.replace("-"," "));
+
+//   $i.isotope({ filter: '.'+filter });
+// }else{
+//   $i.isotope({ filter: '*' });
+// }
