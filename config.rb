14de43b5c62166b713b04a9d046d3571fc3e96d0
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-04-08 21:46:24 +0200

    Upgrade middleman and npm packages

--- config.rb
+++ config.rb
@@ -1,26 +1,47 @@
 require 'uri'
 
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+ignore '/templates/*'
+
+activate :directory_indexes
+# activate :asset_hash
+
+webpack_command =
+  if build?
+    "yarn run build"
+  else
+    "yarn run dev"
+  end
+
 configure :development do
   activate :livereload
+  activate :search_engine_sitemap,
+           default_priority: 0.5,
+           default_change_frequency: "weekly"
 end
 
-activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', 'Explorer >= 8']
-  config.cascade  = false
-  config.inline   = true
+activate :external_pipeline,
+  name: :webpack,
+  command: webpack_command,
+  source: ".tmp/dist",
+  latency: 1
+
+configure :build do
+  activate :minify_html do |html|
+    html.remove_input_attributes = false
+  end
+  activate :search_engine_sitemap,
+    default_priority: 0.5,
+    default_change_frequency: 'weekly'
 end
 
 set :url_root, ''
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :partials_dir, 'partials'
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true,
                footnotes: true, with_toc_data: true
-
-activate :syntax, line_numbers: false
-activate :directory_indexes
 
 set :url_root, 'http://www.podologiafiorentina.it' # as http://demo1.cantierecreativo.net
 
@@ -44,12 +65,4 @@ helpers do
     }
   end
 
-end
-
-
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash # invalida cache degli asset statici
-  activate :search_engine_sitemap, default_priority: 0.5, default_change_frequency: "weekly"
 end
--- source/javascripts/all.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require isotope.pkgd.min
-//= require images_loaded
-//= require isotope_layout
-
-
